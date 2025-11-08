6fd63d9cd814e09409150ad43afd540fc0aa2cc3
Student <artiolthaqi06@gmail.com>
2025-11-08 14:33:25 +0100

    lesson 1

---
+++ moduli1/lists.py
@@ -0,0 +1,25 @@
+to_do_list = ["math homework","Clean the room","resarch the history","fun"]
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+shopping_list = ["apples","bannanas",3,4.5]
+shopping_list.append("oranges")
+
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+to_do_list.remove("resarch the history")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+to_do_list[0] = "physics homework"
+print(to_do_list)
+
+to_do_list[1] = 69
+print(to_do_list)
---
+++ moduli1/variables.py
@@ -0,0 +1,25 @@
+name = "Liridona"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+age = 34
+print("Age is:",age)
+
+x = 5
+y = 8
+result = x + y
+print("Rezultati eshte:" ,result)
+
+age = age + 1
+
+print("Age now is:" ,age)
+
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name +" " + last_name
+
+print(full_name)
