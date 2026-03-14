f35b8fce17e14d44f4efe093308c5d29bc568a80
Artiol Thaqi <artiolthaqi06@gmail.com>
2026-03-14 14:28:38 +0100

    lesson 13

---
+++ Moduli13/pandas_series.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+products = ['Apples','Bananas','Oranges','Grapes','Pineapples']
+sales = [150,200,160,90,60]
+sales_series = pd.Series(sales, index=products)
+print(sales_series)
+print(sales_series['Grapes'])
+
+total_series = sales_series.sum()
+print(total_series)
+
+best_selling_products = sales_series.idxmax()
+print(f"best selling product: {best_selling_products}")
+
